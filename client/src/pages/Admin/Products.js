19662850aa5eb86bb80f61bf7e9469f062e5e556
Abhishek Kumar Bauri <abhishekbauri12@gmail.com>
2024-01-27 11:25:31 +0530

    Listing product on home page

--- client/src/pages/Admin/Products.js
+++ client/src/pages/Admin/Products.js
@@ -31,14 +31,14 @@ const Products = () => {
           </div>
           <div className="col-md-9">
             <h1 className="text-center">All Product List</h1>
-            <div className="d-flex flex-wrap">
+            <div className="d-flex justify-content-around flex-wrap">
               {products?.map((p) => (
                 <Link
                   key={p._id}
                   to={`/dashboard/admin/product/${p.slug}`}
                   className="product-link"
                 >
-                  <div className="card m-2" style={{ width: "18rem" }}>
+                  <div className="card mt-3" style={{ width: "18rem" }}>
                     <img
                       src={`/api/v1/product/product-photo/${p._id}`}
                       className="card-img-top"
--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,13 +1,105 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Layout from "../components/layout/Layout";
-import { useAuth } from "../context/auth";
+import axios from "axios";
+import { Checkbox, Radio } from "antd";
+import { Prices } from "../components/Prices";
 
 const HomePage = () => {
-  const [auth, setAuth] = useAuth();
+  const [products, setProducts] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [checked, setChecked] = useState([]);
+  const [radio, setRadio] = useState([]);
+
+  // Get all categories
+  const getAllCategory = async () => {
+    try {
+      const { data } = await axios.get("/api/v1/category/categories");
+      if (data?.status === "success") {
+        setCategories(data?.category);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // get products
+  const getAllProducts = async () => {
+    try {
+      const { data } = await axios.get("/api/v1/product/get-product");
+      setProducts(data.products);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getAllCategory();
+    getAllProducts();
+  }, []);
+
+  const handleFilter = (value, id) => {
+    let all = [...checked];
+    if (value) {
+      all.push(id);
+    } else {
+      all = all.filter((c) => c !== id);
+    }
+    setChecked(all);
+  };
   return (
-    <Layout title={"Best offers"}>
-      <h1> home page</h1>
-      <pre>{JSON.stringify(auth, null, 4)}</pre>
+    <Layout title={"All Products - Best offers"}>
+      <div className="container-fluid mt-3 p3">
+        <div className="row">
+          <div className="col-md-2">
+            <h4 className="text-center">Filter By Category</h4>
+            <div className="d-flex flex-column">
+              {categories?.map((c) => (
+                <Checkbox
+                  key={c._id}
+                  onChange={(e) => handleFilter(e.target.checked, c._id)}
+                >
+                  {c.name}
+                </Checkbox>
+              ))}
+            </div>
+
+            <h4 className="text-center mt-4">Filter By Price</h4>
+            <div className="d-flex flex-column">
+              <Radio.Group onChange={(e) => setRadio(e.target.value)}>
+                {Prices?.map((p) => (
+                  <div key={p._id}>
+                    <Radio value={p.array}>{p.name}</Radio>
+                  </div>
+                ))}
+              </Radio.Group>
+            </div>
+          </div>
+          <div className="col-md-10">
+            <h1 className="text-center">All Products</h1>
+            <div className="d-flex justify-content-around flex-wrap">
+              {products?.map((p) => (
+                <div className="card mt-3" style={{ width: "18rem" }}>
+                  <img
+                    src={`/api/v1/product/product-photo/${p._id}`}
+                    className="card-img-top"
+                    alt={p.name}
+                  />
+                  <div className="card-body">
+                    <h5 className="card-title">{p.name}</h5>
+                    <p className="card-text">{p.description}</p>
+                    <button className="btn btn-primary ms-1">
+                      More Details
+                    </button>
+                    <button className="btn btn-secondary ms-1">
+                      Add To Cart
+                    </button>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 };

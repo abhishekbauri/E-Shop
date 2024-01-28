6c4014e352511b698098abaedc464a6fc88d425c
Abhishek Kumar Bauri <abhishekbauri12@gmail.com>
2024-01-28 12:05:57 +0530

    Product details of Item

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -3,8 +3,10 @@ import Layout from "../components/layout/Layout";
 import axios from "axios";
 import { Checkbox, Radio } from "antd";
 import { Prices } from "../components/Prices";
+import { useNavigate } from "react-router-dom";
 
 const HomePage = () => {
+  const navigate = useNavigate();
   const [products, setProducts] = useState([]);
   const [categories, setCategories] = useState([]);
   const [checked, setChecked] = useState([]);
@@ -161,7 +163,10 @@ const HomePage = () => {
                       {p.description.substring(0, 30)}...
                     </p>
                     <p className="card-text">$ {p.price}</p>
-                    <button className="btn btn-primary ms-1">
+                    <button
+                      className="btn btn-primary ms-1"
+                      onClick={() => navigate(`/product/${p.slug}`)}
+                    >
                       More Details
                     </button>
                     <button className="btn btn-secondary ms-1">

0d4c2dbabe19880c3faddb6fd58d55bc6bffb5a4
Abhishek Kumar Bauri <78651887+abhishekbauri@users.noreply.github.com>
2024-02-23 15:26:06 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ const app = express();
 
 // middleware
 const corsOptions = {
-  origin: "https://e-shop-by-akb.vercel.app/",
+  origin: "https://e-shop-by-akb.vercel.app",
 };
 
 app.use(cors(corsOptions));
